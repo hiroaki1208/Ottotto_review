b8c1d27ee57afdc0cb32ccb5cf546f138483cffa
ikamirin <hiroaki_ikami@yahoo.co.jp>
2023-06-03 17:17:15 +0900

    logging追加

--- run.py
+++ run.py
@@ -1,8 +1,30 @@
 import pandas as pd
 import numpy as np
 
+import os, sys
+import argparse
+import logging.config
+
 def main():
+
+    base_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__))
+    )
     print('main')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', default='validation'
+                        , help= 'execution type(validation or test)'
+                        , choices= ['validation', 'test'])
+    args = parser.parse_args()
+
+    try:
+        logging.config.fileConfig(os.path.join(base_dir, 'logs', 'logging.ini'))
+        logging.info(f'start: {base_dir}')
+    except Exception as e:
+        logging.exception(e)
+        sys.exit(1)
+
 
     
 
