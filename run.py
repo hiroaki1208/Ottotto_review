a493bbc8ebcaa5ae34f93318112fa042fd44dc6a
ikamirin <hiroaki_ikami@yahoo.co.jp>
2023-06-06 00:07:52 +0900

    param読み込みをymlへ変更

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ import src.prediction
 
 DATA_DIR = os.getenv('DATA_DIR')
 OUTPUT_DIR = os.getenv('OUTPUT_DIR')
+CONFIG_DIR = os.getenv('CONFIG_DIR')
 LOG_DIR = os.getenv('LOG_DIR')
 
 def create_result_config(result_dir, args):
@@ -20,6 +21,16 @@ def create_result_config(result_dir, args):
     with open(result_path, 'w') as rp:
         yaml.dump(vars(args), rp, default_flow_style=False)
 
+def _load_config_file(fname):
+    '''parameter設定ファイルを読み込み
+    '''
+    logging.info(f'start loading parameter configuration')
+    with open(os.path.join(CONFIG_DIR, 'parameter', f'{fname}.yml')) as file:
+        config = yaml.safe_load(file.read())
+    # config = config_covisit[self.setting_name]
+    logging.info(f'end loading parameter configuration')
+
+    return config
 
 def main():
 
@@ -30,44 +41,50 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--subfolder')
-    parser.add_argument('-t', '--target', default='validation'
-                        , help= 'execution type(validation or test)'
-                        , choices= ['validation', 'test'])
+    # parser.add_argument('-t', '--target', default='validation'
+    #                     , help= 'execution type(validation or test)'
+    #                     , choices= ['validation', 'test'])
     # parser.add_argument('-p', '--is_partial', default=1
     #                     , help= 'train data partial or not', type=int
     #                     )
-    parser.add_argument('-c', '--covisit', default=1
-                        , help= 'config of co-visitation matrix'
+    parser.add_argument('-p', '--param_idx', default='param0'
+                        , help= 'parameter index', type=str
                         )
+    # parser.add_argument('-c', '--covisit', default=1
+    #                     , help= 'config of co-visitation matrix'
+    #                     )
     args = parser.parse_args()
+
+    param_config = _load_config_file(args.param_idx)
+    print(param_config)
 
     try:
         logging.basicConfig(level=logging.INFO)   
         logging.config.fileConfig(os.path.join(base_dir, 'logs', 'logging.ini')
                                   , defaults={'logdir': LOG_DIR})
         
-        # 計算設定保存
-        result_config_dir = os.path.join(OUTPUT_DIR, 'result', args.subfolder)
-        os.mkdir(result_config_dir)
-        create_result_config(result_config_dir, args)
+        # # 計算設定保存
+        # result_config_dir = os.path.join(OUTPUT_DIR, 'result', args.subfolder)
+        # os.mkdir(result_config_dir)
+        # create_result_config(result_config_dir, args)
 
-        logging.info(f'start: {base_dir}')
-        logging.info(f'(param)target: {args.target}')
-        # logging.info(f'(param)is_partial: {args.is_partial}')
-        logging.info(f'(param)covisitation setting: {args.covisit}')
+        # logging.info(f'start: {base_dir}')
+        # logging.info(f'(param)target: {args.target}')
+        # # logging.info(f'(param)is_partial: {args.is_partial}')
+        # logging.info(f'(param)covisitation setting: {args.covisit}')
 
-        # 特徴量
+        # # 特徴量
 
-        # create model
-        ## co-visitaion
-        CoVisitaion = models.create_covisit.CreateCoVisitaion(
-            args.covisit, TYPE_LABEL
-            )
-        CoVisitaion.main()
+        # # create model
+        # ## co-visitaion
+        # CoVisitaion = models.create_covisit.CreateCoVisitaion(
+        #     args.covisit, TYPE_LABEL
+        #     )
+        # CoVisitaion.main()
 
-        # prediction
-        Prediction = src.prediction.CreatePrediction(vars(args), TYPE_LABEL)
-        Prediction.main()
+        # # prediction
+        # Prediction = src.prediction.CreatePrediction(vars(args), TYPE_LABEL)
+        # Prediction.main()
 
 
 
--- run.sh
+++ run.sh
@@ -4,17 +4,20 @@ BASE_DIR=$(dirname ${0})
 cd ${BASE_DIR}
 
 # defaultパラメータ設定
-TARGET='validation'
+# TARGET='validation'
 # IS_PARTIAL=0
-COVISIT='setting0'
+# COVISIT='setting0'
+PARAM='param0'
 SUBFOLDER=$(date '+%Y%m%d%H%M')
 
 # パラメータのパース
-while getopts t:c: OPT; do
+# while getopts t:p:c: OPT; do
+while getopts p: OPT; do
     case ${OPT} in
-        t) TARGET=${OPTARG} ;;
+        # t) TARGET=${OPTARG} ;;
         # p) IS_PARTIAL=1 ;;
-        c) COVISIT=${OPTARG} ;;
+        p) PARAM=${OPTARG} ;;
+        # c) COVISIT=${OPTARG} ;;
         *) usage ;;
     esac
 done
@@ -23,4 +26,4 @@ source ${BASE_DIR}/configs/config.sh
 set -euC
 # conda activate env2
 
-python ${BASE_DIR}/run.py -s ${SUBFOLDER} -t ${TARGET} -c ${COVISIT}
+python ${BASE_DIR}/run.py -s ${SUBFOLDER} -p ${PARAM}
